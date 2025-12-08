a17006f6f9c6be6dbab41327c86ac344dee5f6fd
NguyenAnhMy242712 <nguyenanhmy242712@gmail.com>
2025-12-08 10:28:32 +0700

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -1,64 +1,66 @@
 <?php 
+$page = basename($_SERVER['PHP_SELF']);
+?>
 
-$page= substr($_SERVER['SCRIPT_NAME'], strripos($_SERVER['SCRIPT_NAME'],"/")+1);
- ?>
+<head>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="./css/header.css">
+</head>
 
-<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
-    <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="#" target="_blank">
-        <span class="ms-1 font-weight-bold text-white">Car-selling DASHBOAR</span>
-      </a>
+<aside class="sidenav" id="sidenav-main">
+
+    <!-- Header -->
+    <div class="sidenav-header text-center">
+        <span class="navbar-brand text-white">CAR SELLING MANAGE</span>
     </div>
-    <hr class="horizontal light mt-0 mb-2">
-    <div class="collapse navbar-collapse  w-auto" id="sidenav-collapse-main" style="height: 75vh">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link text-white <?= $page== "index.php"? 'active bg-gradient-primary':'' ?>" href="../admin/index.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">dashboard</i>
-            </div>
-            <span class="nav-link-text ms-1">Dashboard</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white <?= $page== "customer.php"? 'active bg-gradient-primary':'' ?>" href="customer.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">person</i>
-            </div>
-            <span class="nav-link-text ms-1">Customer manage</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white <?= $page== "order.php"? 'active bg-gradient-primary':'' ?>" href="order.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">weekend</i>
-            </div>
-            <span class="nav-link-text ms-1">Order manage</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white <?= $page== "verhical.php"? 'active bg-gradient-primary':'' ?>" href="verhicall.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">table_view</i>
-            </div>
-            <span class="nav-link-text ms-1">Verhical manage</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white <?= $page== "manufacturer.php"? 'active bg-gradient-primary':'' ?>" href="manufacturer.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">table_view</i>
-            </div>
-            <span class="nav-link-text ms-1">Manufacturers manage</span>
-          </a>
-        </li>
-        <!--  -->
-      </ul>
+
+    <hr class="horizontal">
+
+    <!-- Main menu -->
+    <div class="collapse" id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link <?= $page == "index.php" ? 'active' : '' ?>" href="../admin/index.php">
+                    <div><i class="fas fa-chart-line"></i></div>
+                    <span class="nav-link-text">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link <?= $page == "customer.php" ? 'active' : '' ?>" href="customer.php">
+                    <div><i class="fas fa-users"></i></div>
+                    <span class="nav-link-text">Customer manage</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link <?= $page == "order.php" ? 'active' : '' ?>" href="order.php">
+                    <div><i class="fas fa-shopping-cart"></i></div>
+                    <span class="nav-link-text">Order manage</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link <?= $page == "vehicle.php" ? 'active' : '' ?>" href="vehicle.php">
+                    <div><i class="fas fa-car"></i></div>
+                    <span class="nav-link-text">Vehicle manage</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link <?= $page == "manufacturer.php" ? 'active' : '' ?>" href="manufacturer.php">
+                    <div><i class="fas fa-industry"></i></div>
+                    <span class="nav-link-text">Manufacturers manage</span>
+                </a>
+            </li>
+
+        </ul>
     </div>
-    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-      <div class="mx-3">
-        <a class="btn bg-gradient-primary mt-4 w-100" href="../logout.php" type="button">Logout</a>
-      </div>
+
+    <!-- Footer -->
+    <div class="sidenav-footer text-center">
+        <a class="btn bg-gradient-primary text-white w-75" href="../logout.php">Logout</a>
     </div>
+
 </aside>
