9a9f270d8ca3eaec3c892cb74ef008264e003c3f
NguyenAnhMy242712 <nguyenanhmy242712@gmail.com>
2025-12-08 08:07:26 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php 
-include ("../admin/includes/header.php");
+//include ("../admin/includes/header.php");
 ?>
 <body>
 <div class="container">
@@ -16,7 +16,7 @@ include ("../admin/includes/header.php");
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">Total Users</p>
-                        <h4 class="mb-0"><?= totalValue('users') ?></h4>
+                        <h4 class="mb-0"><?= totalValue('customer') ?></h4>
                     </div>
                     </div>
                     <hr class="dark horizontal my-0">
@@ -32,8 +32,25 @@ include ("../admin/includes/header.php");
                         <i class="material-icons opacity-10">table_view</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Total Product</p>
-                        <h4 class="mb-0"><?= totalValue('products') ?></h4>
+                        <p class="text-sm mb-0 text-capitalize">Total Vehicles</p>
+                        <h4 class="mb-0"><?= totalValue('vehicle') ?></h4>
+                    </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than lask week</p>
+                    </div>
+                </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-header p-3 pt-2">
+                    <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">Total Manufacturers</p>
+                        <h4 class="mb-0"><?= totalValue('manufacturer') ?></h4>
                     </div>
                     </div>
                     <hr class="dark horizontal my-0">
@@ -50,7 +67,7 @@ include ("../admin/includes/header.php");
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">Total order</p>
-                        <h4 class="mb-0"><?= totalValue('orders') ?></h4>
+                        <h4 class="mb-0"><?= totalValue('order') ?></h4>
                     </div>
                     </div>
                     <hr class="dark horizontal my-0">
@@ -59,26 +76,10 @@ include ("../admin/includes/header.php");
                     </div>
                 </div>
                 </div>
-                <div class="col-xl-3 col-sm-6">
-                <div class="card">
-                    <div class="card-header p-3 pt-2">
-                    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">weekend</i>
-                    </div>
-                    <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Sales</p>
-                        <h4 class="mb-0">$<?= totalPriceGet() ?></h4>
-                    </div>
-                    </div>
-                    <hr class="dark horizontal my-0">
-                    <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than yesterday</p>
-                    </div>
-                </div>
-                </div>
+                
             </div>
         </div>
     </div>
 </div>
 </body>
-<?php include ("../admin/includes/footer.php"); ?>
+<?php //include ("../admin/includes/footer.php"); ?>
