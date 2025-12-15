1ce3677448afb06422adf21e81042e25202d56da
NguyenAnhMy242712 <nguyenanhmy242712@gmail.com>
2025-12-15 13:17:14 +0700

    Update dbconn.php

--- admin/dbconn.php
+++ admin/dbconn.php
@@ -3,8 +3,25 @@ $link = mysqli_connect("localhost", "root", "") or die(mysqli_error($link));
 mysqli_select_db($link, "car_selling") or die(mysqli_error($link));
 function getAllCustomers() {
     global $link;
-    $sql = "SELECT * FROM customer";
+
+    $sql = "
+        SELECT 
+            c.customer_id,
+            c.name,
+            c.phone_number,
+            c.email,
+            c.age,
+            c.dob,
+            COUNT(o.order_id) AS total_orders
+        FROM customer c
+        LEFT JOIN orders o 
+            ON c.customer_id = o.customer_id
+        GROUP BY c.customer_id
+    ";
+
     $query = mysqli_query($link, $sql);
     return mysqli_fetch_all($query, MYSQLI_ASSOC);
-}
+    }
+
+$users = getAllCustomers();
 ?>
