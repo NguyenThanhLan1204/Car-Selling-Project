985308491d58ace701fe380d39b0aa3ca2005cb0
NguyenAnhMy242712 <nguyenanhmy242712@gmail.com>
2025-12-23 22:07:09 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,19 @@
 <?php
 session_start();
-//Xóa giỏ hàng trong session
+
+// Xóa cookie giỏ hàng theo user (QUAN TRỌNG NHẤT)
+if (isset($_SESSION['customer_id'])) {
+    $customer_id = $_SESSION['customer_id'];
+    setcookie('user_cart_' . $customer_id, '', time() - 3600, '/');
+}
+
+// Xóa giỏ hàng trong session
 unset($_SESSION['cart']);
-//Xóa session
+
+// Xóa toàn bộ session
 $_SESSION = [];
-// Xóa cookie session (PHPSESSID)
+
+// Xóa cookie PHPSESSID
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -12,20 +21,15 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
+
+// Hủy session
 session_destroy();
 
-// Xóa cookie remember_login (quan trọng)
+// Xóa các cookie khác
 setcookie("remember_login", "", time() - 3600, "/");
-
-// Xóa cookie username (tùy chọn)
 setcookie("username", "", time() - 3600, "/");
-
-// Xóa cookie role
 setcookie("role", "", time() - 3600, "/");
-
-// Xóa cookie name
 setcookie("name", "", time() - 3600, "/");
 
 header("Location: base.php?page=home");
 exit();
-?>
